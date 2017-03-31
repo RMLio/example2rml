a2937e19c0f553da42f7dfd7e671e95b11a4ab60
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-03-31 16:04:15 +0200

    add docs

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ let SemanticModelGenerator = require('./lib/semanticmodelgenerator.js');
 let RMLMappingGenerator = require('semanticmodel').RMLMappingGenerator;
 let Q = require('q');
 
+/**
+ * Generate an RML mapping from a given set of triples and data sources.
+ * @param triples: the triples to consider when generating the mapping
+ * @param dataSources: the data sources from which the triples are being mapped
+ * @returns {promise}: the promise that will resolve once the RML mapping is generated (RDF triples are provided)
+ */
 function generate(triples, dataSources){
   let deferred = Q.defer();
   let smg = new SemanticModelGenerator(triples);
   let aligner = new Aligner(dataSources);
   let rmg = new RMLMappingGenerator({baseIRI: 'http://www.mymapping.com#'});
 
+  //1. gemerate the semantic model
   smg.getModel().then(function(sm) {
+    //2. align the semantic model with the data sources
     aligner.align(sm);
+    //3. generate RML from the semantic model
     deferred.resolve(rmg.generate(sm));
   });
 
