8e5a5979b0f4a6b5915df57e03f886d933dda999
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-06-14 11:09:03 +0200

    extra test

--- test/json.js
+++ test/json.js
@@ -6,6 +6,7 @@ let assert = require('chai').assert;
 let example2rml = require('../index.js');
 let type = require('semanticmodel').nodeType.types;
 let N3 = require('n3');
+const utils = require('../lib/utils.js');
 
 describe('JSON:', function () {
   it('single entity with attributes', function () {
@@ -208,6 +209,57 @@ describe('JSON:', function () {
     return example2rml(triples, dataSources).
     catch(function(e){
       assert.equal(e.name, 'InvalidIteratorError', 'Error is incorrect.');
+    });
+  });
+
+  it('extra test', function () {
+    this.timeout(10000);
+    let triples = [
+      {
+        subject: 'http://www.example.com/person/0',
+        predicate: 'http://www.example.com/name',
+        object: '"John"'
+      },
+      {
+        subject: 'http://www.example.com/person/0',
+        predicate: 'http://www.example.com/name',
+        object: '"30"'
+      },
+      {
+        subject: 'http://www.example.com/person/0',
+        predicate: 'http://www.example.com/friend',
+        object: 'http://www.example.com/friend/Luke'
+      }
+    ];
+
+    //the column names need to have double quotes around the header
+    let dataSources = [{
+      sourceDescription: {
+        type: 'json',
+        source: 'person.json'
+      },
+      object: {
+        "persons": [
+          {
+            "ID": 0,
+            "name": "John",
+            "age": "30",
+            "friend_id": 1
+          },
+          {
+            "ID": 1,
+            "name": "James",
+            "age": "25",
+            "friend_id": 2
+          }
+        ]
+      }
+    }];
+
+    return example2rml(triples, dataSources).then(function (rml) {
+      //console.log(JSON.stringify(rml));
+      assert.deepEqual(rml, require('./index.json').mappings[5], 'RML triples are not correct.');
+      //utils.showReadableRML(rml);
     });
   });
 });
